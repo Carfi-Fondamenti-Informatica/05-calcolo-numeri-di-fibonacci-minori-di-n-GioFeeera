ed86e120c2955c0dea9520a4e6d410a2e7763491
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-11-27 17:31:55 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, a=1, b=0, c=0;
+    cin>> n;
+    if (n>0) {
+        cout << a << endl;
+        while (c<n) {
+            c=a+b;
+            b=a;
+            a=c;
+            if (c<=n) {
+                cout << c << endl;
+            }
+        }
+
+    }
+
+    return 0;
 }
